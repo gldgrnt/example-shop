a7d215bf2bbafc36bc5e468dc16adca78bd6c73c
Gilad Grant <ggrant966@gmail.com>
2022-03-10 21:15:16 +0000

    feat: load product data into views

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,28 +1,31 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import { Container } from "components/shared/Container";
 import { Header } from "components/Header";
 import { TView } from "types/views";
+import { BasketProvider } from "state/basket";
 
 import ShopView from "./ShopView";
 import BasketView from "./BasketView";
+import { getProductData } from "helpers/product";
 
 const App = () => {
   const [view, setView] = useState<TView>("shop");
+  const productData = useMemo(() => getProductData(), []);
 
   return (
-    <>
+    <BasketProvider>
       <Header view={view} setView={setView} />
       <main style={{ padding: "2rem 0" }}>
         <Container>
-          {view === "shop" && <ShopView />}
-          {view === "basket" && <BasketView />}
+          {view === "shop" && <ShopView productData={productData} />}
+          {view === "basket" && <BasketView productData={productData} />}
         </Container>
       </main>
       <footer style={{ padding: "1rem 0" }}>
         <Container>&copy; Example store</Container>
       </footer>
-    </>
+    </BasketProvider>
   );
 };
 
