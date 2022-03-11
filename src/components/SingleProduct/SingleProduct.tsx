750e47ddb9a5dcc9c08991aadcc207a7cfc83ae8
Gilad Grant <ggrant966@gmail.com>
2022-03-11 12:12:08 +0000

    feat: add basket state function to product

--- src/components/SingleProduct/SingleProduct.tsx
+++ src/components/SingleProduct/SingleProduct.tsx
@@ -1,9 +1,18 @@
 import { Button } from "@mui/material";
 import { formatPrice } from "helpers/product";
+import { useCallback } from "react";
+import { useBasketContext } from "state/basket";
+import { EBasketActions } from "state/basket/basket.types";
 
 import { IProps } from "./SingleProduct.types";
 
 const SingleProduct = ({ product }: IProps) => {
+  const { dispatch } = useBasketContext();
+
+  const addToBasket = useCallback(() => {
+    dispatch({ type: EBasketActions.ADD_TO_BASKET, payload: product.id });
+  }, [dispatch, product.id]);
+
   return (
     <div
       style={{
@@ -12,8 +21,8 @@ const SingleProduct = ({ product }: IProps) => {
       }}
     >
       <h3 style={{ textTransform: "capitalize" }}>{product.name}</h3>
-      <p style={{ marginTop: 0 }}>Price: {formatPrice(product.price)}</p>
-      <Button variant="outlined" onClick={() => console.log("hello")}>
+      <p style={{ marginTop: 0 }}>Price: {formatPrice(product.unitPrice)}</p>
+      <Button variant="outlined" onClick={addToBasket}>
         Add to basket
       </Button>
       <p style={{ padding: "1rem", backgroundColor: "#F3F3F3" }}>
