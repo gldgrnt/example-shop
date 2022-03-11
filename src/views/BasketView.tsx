af426c2fd80b5fb5d55459a85c63edf38c6fb0e3
Gilad Grant <ggrant966@gmail.com>
2022-03-11 12:11:27 +0000

    feat: show BasketTable in BasketView

--- src/views/BasketView.tsx
+++ src/views/BasketView.tsx
@@ -1,5 +1,24 @@
-const BasketView = () => {
-  return <div>This is the basket</div>;
+import { useMemo } from "react";
+
+import { BasketTable } from "components/BasketTable";
+import { createBasketTableRows } from "helpers/basket";
+import { useBasketContext } from "state/basket";
+import { IViewProps } from "types/views";
+
+const BasketView = ({ productData }: IViewProps) => {
+  const { state } = useBasketContext();
+  const isBasketEmpty = state.length < 1;
+
+  const rows = useMemo(() => {
+    return createBasketTableRows(state, productData);
+  }, [productData, state]);
+
+  return (
+    <section>
+      <h2 style={{ marginTop: 0 }}>Basket</h2>
+      {isBasketEmpty ? <h3>Basket is empty</h3> : <BasketTable rows={rows} />}
+    </section>
+  );
 };
 
 export default BasketView;
